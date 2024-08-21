79015aa92b8816c52846ff54a6df893bd76785fb
Maximo <gaticamaxiimo2@gmail.com>
2024-08-21 11:24:26 -0300

    add models to admin site

--- cinemaTicket/models.py
+++ cinemaTicket/models.py
@@ -6,11 +6,17 @@ class CinemaRoom(models.Model):
     name = models.CharField(max_length=20)
     capacity = models.SmallIntegerField()
 
+    def __str__(self):
+        return self.name
+
 
 class Movie(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField()
     poster_image = models.ImageField(upload_to="posters/")
+
+    def __str__(self):
+        return self.name
 
 
 class Projection(models.Model):
@@ -21,5 +27,4 @@ class Projection(models.Model):
 
     def __str__(self):
         return f"projection_date: {self.projection_date}, \
-        cinema_room: {self.cinema_rooms}, \
-        created_at: {self.created_at}, movie: {self.movie}"
+        movie: {self.movie.name}"
